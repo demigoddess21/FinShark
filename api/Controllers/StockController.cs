94ceb5f23ff2ecd627a2cb1a33707d9adf138a98
demigoddes21 <aaradhanaiyer21@gmail.com>
2024-05-15 19:12:12 +0400

    first commit

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var stocks = await _context.Stocks.ToListAsync();
+            var stockDto = stocks.Select(s => s.ToStockDto());
+            return Ok(stockDto);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDto();
+
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.MarketCap = updateDto.MarketCap;
+            stockModel.Industry = updateDto.Industry;
+
+            await _context.SaveChangesAsync();
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+
+            _context.Stocks.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Model;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                MarketCap = stockModel.MarketCap,
+                Industry = stockModel.Industry
+
+
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockModel)
+        {
+            return new Stock
+            {
+
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                MarketCap = stockModel.MarketCap,
+                Industry = stockModel.Industry
+
+
+            };
+        }
+
+    }
+}
